5af6017aa2050cd8e576af11f8ce1ae122e13af1
Dariusz Wytrykus <d.wytrykus@gmail.com>
2021-04-07 13:09:49 +0200

    Add API

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
-<template>
+ <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <pre>{{ response }}</pre>
+    </div>
   </div>
 </template>
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'About',
+  data () {
+    return {
+      response: null
+    }
+  },
+  mounted () {
+    this.fetchPizzas()
+  },
+  methods: {
+    async fetchPizzas () {
+      this.response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(({ data }) => data)
+    }
+  }
+}
+</script>
